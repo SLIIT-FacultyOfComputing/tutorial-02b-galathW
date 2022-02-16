04ada97c773deb09c677617e0182ce3633942fc6
Venura Kaushan <95140756+galathW@users.noreply.github.com>
2022-02-16 18:53:16 +0000

    convert to c++

--- tute04.cpp
+++ tute04.cpp
@@ -11,7 +11,7 @@ long Factorial(int no);
 long nCr(int n, int r);
 Do not modify the main function.*/
 
-#include <iostream>
+/*#include <iostream>
 
 long Factorial(int no);
 long nCr(int n, int r);
@@ -26,4 +26,49 @@ int main() {
   std::cout << nCr(n,r);
   std::cout << std::endl;
   return 0;
+}*/
+
+#include <iostream>
+
+long factorial(int no);
+long nCr(int n, int r);
+
+int main()
+{
+  int n, r;
+  std::cout << "Enter a value for n ";
+  std::cin >> n;
+
+  std::cout << "Enter a value for r ";
+  std::cin >> r;
+
+  std::cout << "nCr = ";
+  std::cout << nCr(n,r);
+  std::cout << std::endl;
+  return 0;
+}
+
+long nCr(int n, int r)
+{
+   return 
+}
+long Factorial(int no)
+{
+  int fact =1;
+  for(int i = no; i >= 1; --i)
+  {
+    fact *= i;
+  }
+  return fact;
+}
+
+long nCr(int n, int r)
+{
+  int final_output = 0;
+  int factorial_n = Factorial(n);
+  int factorial_r = Factorial(r);
+
+  final_output = factorial_n / factorial_r * Factorial((n - r));
+
+  return final_output;
 }
